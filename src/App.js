bb3a848dddf30555f54af6feca3cda3bf1ba989f
Ha young, Kim <jeremiah91@naver.com>
2021-04-15 12:59:35 +0900

    creating my first component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+// how react works :
+// 1) App.js 에서 코드 작성 --> index.js 가 App.js 를 index.html로 전달해줌
+// 또한, 이 과정에서 실제 index.html파일에 추가되지는 않지만 local에서 내가 작성한 코드를 확인 가능
+// 즉, virtual DOM 을 활용하여 실시간으로 재렌더링해줌
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      hello! I am hayoung :)
     </div>
   );
 }
---
+++ src/MyComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MyComponent() {
+    return (
+        <div>
+            <h3>This is my first component</h3>
+        </div>
+    );
+};
+
+export default MyComponent;
