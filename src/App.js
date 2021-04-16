2780be34957fe6709ad6de8f6f78135c11430af5
Ha young, Kim <jeremiah91@naver.com>
2021-04-16 16:56:50 +0900

    learning life-cycle of components

--- src/App.js
+++ src/App.js
@@ -1,32 +1,26 @@
 import React from "react";
-import PropTypes from "prop-types";
 
-// class component는 function component와 달리 함수가 아니기 때문에, 안에 render 함수를 작성해줘야함.
-// class component를 사용하는 가장 큰 이유는 state때문
-// state에는 component에서 사용할 '가변적 데이터'를 저장하는 곳
-// 가변적이라고해서 state 안의 데이터를 직접 변경해서는 안된다. 반드시 state 안의 변경함수를 사용하여 변경함
 class App extends React.Component{
-  // state 는 결국 object이다.
-  state={
-    count: 0
+  constructor(){
+    console.log("1) I'm ready to be rendered ");
   };
-
-  add = () =>{
-    this.setState(current => ({count: current.count + 1}));
+  componentDidMount(){
+    console.log("2) after render(), 3) Now, I'm rendered ");
   };
-  minus = () =>{
-    // setState 내에서 this.state를 사용해서 외부 데이터의 변화에 의존하는 방식은 나중에 문제가 됨
-    // this.setState({count: this.state.count -1});
-    this.setState(current => ({count: current.count - 1}));
+  componentDidUpdate(){
+    console.log("1) after render(), 2) Now, I'm updated ");
+  };
+  componentWillUnmonut(){
+    console.log("this is called when a component is being removed from the DOM ");
   };
   render(){
+    // 매번 this.state를 사용해서 data를 가져오기보다는, ES6 문법을 사용해서 가져온다.
+    const { isLoading } = this.state;
     return (
       <div>
-        <h1>The number : {this.state.count} </h1>
-        <button onClick={this.add}> plus + </button>
-        <button onClick={this.minus}> minus - </button>
+        {isLoading ? "Loading..." : "We are ready"}
       </div>
-    )
+    );
   }
 
 
