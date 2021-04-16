c908927c131407d8d8a83e3f76c6592946891344
Ha young, Kim <jeremiah91@naver.com>
2021-04-16 11:23:31 +0900

    learning props 2

--- src/App.js
+++ src/App.js
@@ -3,24 +3,53 @@
 // 또한, 이 과정에서 실제 index.html파일에 추가되지는 않지만 local에서 내가 작성한 코드를 확인 가능
 // 즉, virtual DOM 을 활용하여 실시간으로 재렌더링해줌
 
+// 가상의 data 설정 :
+const virtual_data = [
+  {
+    id: 0,
+    name: "data_01",
+    contents: "Lorem, ipsum dolor sit amet consectetur adipisicing elit."
+  },
+  {
+    id: 1,
+    name: "data_02",
+    contents: "Voluptas deleniti natus excepturi voluptates voluptate autem dolorem culpa sequi fugiat doloremque nulla ab totam, aut facere alias, tempora ut. Totam, dolore."
+  },
+  {
+    id: 2,
+    name: "data_03",
+    contents: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas deleniti natus excepturi voluptates voluptate autem dolorem culpa sequi fugiat doloremque nulla ab totam, aut facere alias, tempora ut. Totam, dolore."
+  },
+  {
+    id: 3,
+    name: "data_04",
+    contents: "Voluptas deleniti natus excepturi Lorem, ipsum dolor sit amet consectetur adipisicing elit. Lorem, ipsum dolor sit amet consectetur adipisicing elit. voluptates voluptate autem dolorem culpa sequi fugiat doloremque nulla ab totam, aut facere alias, tempora ut. Totam, dolore."
+  },
+  {
+    id: 4,
+    name: "data_05",
+    contents: "Voluptas deleniti natus excepturi voluptates voluptate autem dolorem culpa sequi fugiat doloremque nulla ab totam, aut facere alias, tempora ut. Totam, dolore."
+  },
+];
+
 // props 를 활용하여 component 에서 children component 에게 정보를 보내보자
-// App 에서 정보를 MyComponent로 보낼꺼야
-function MyComponent ({ checkMessage }){
+// 부모에서 정보를 자식으로 보낼꺼야
+function ShowingData ({dataName, dataContents}){
   return (
     <>
-      <div>delivering props successfully : { checkMessage }</div>
+      <div>
+        <div>{ dataName }</div>
+        <div>{ dataContents }</div>
+      </div>
     </>
   )
 }
-
 function App() {
   return (
     <div className="App">
-      hello! I am hayoung :)
-      <MyComponent checkMessage="this is props value" />
-      <MyComponent checkMessage="and can be used" />
-      <MyComponent checkMessage="again and again" />
-      <MyComponent checkMessage="with only one component" />
+      <div>
+        {virtual_data.map(dataset => <ShowingData dataName={dataset.name} dataContents={dataset.contents} />)}
+      </div>
     </div>
   );
 }
