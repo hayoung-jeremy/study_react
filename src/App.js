5b611aa2a7be0f1a183077366ea824c246d11144
Ha young, Kim <jeremiah91@naver.com>
2021-04-19 14:18:27 +0900

    recovering deleted App.js and using Router

--- src/App.js
+++ src/App.js
@@ -1,58 +1,19 @@
-import React from "react";
-import axios from "axios";
-import Movie from "./Movie";
-
-import "./App.css";
-
-class App extends React.Component{
-  state = {
-    isLoading: true,
-    movies: []
-  };
-
-  // 데이터를 가져오는 방식, 즉 axios.get이 느릴수 있으니, rendering 이전에, componentDidMount 사이클에서
-  // async 그리고 await을 사용한다 : await의 작업이 끝날때까지 기다렸다가, async 함수를 실행한다는 뜻
-  getMovies = async () => {
-    // ES6
-    const {data: {data :{movies}}} = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating");
-
-    //this.setState({movies(from state):movies(from axios)})
-    this.setState({ movies, isLoading:false });
-  }
-  componentDidMount(){
-    this.getMovies();
-  }
-
-  render(){
-    // 매번 this.state를 사용해서 data를 가져오기보다는, ES6 문법을 사용해서 가져온다.
-    const { isLoading, movies } = this.state;
-    return (
-      <section className="container">
-        {isLoading
-          ? (<div className="loader">
-            <span className="loader__text">Loading ...</span>
-          </div>)
-          : (
-            <div className="movies">
-              {movies.map(movie => (
-                <Movie 
-                  key={movie.id}
-                  id={movie.id}
-                  year={movie.year}
-                  title={movie.title}
-                  summary={movie.summary}
-                  poster={movie.medium_cover_image}
-                  genres={movie.genres}
-                />
-              ))}
-            </div>
-          )
-        }
-      </section>
-    );
-  }
+import React from 'react';
+import { HashRouter, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
 
 
+function App () {
+  return (
+    <HashRouter>
+      {/* 만약 /about로 접속할 경우, About component를 보여주어라 */}
+      
+      <Route path="/" component={Home} />
+      <Route path="/about" component={About} />
+
+    </HashRouter>
+  )
 }
 
 export default App;
---
+++ src/routes/About.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function About (){
+    return <span>About this page : I built it by using Route </span>
+}
+
+export default About;
